9e036ecba7c29df284ccc62b495587bacdf6813a
z3z1ma <butler.alex2010@gmail.com>
2022-07-18 10:56:43 -0700

    include all databases and ts based determination config based

--- tap_mongodb/tap.py
+++ tap_mongodb/tap.py
@@ -25,15 +25,11 @@ singer.messages.format_message = lambda message: orjson.dumps(
 
 class CollectionStream(Stream):
     primary_keys = ["_id"]
-
-    # TODO: Yet to find a way to dynamically set this in a schemaless
-    # set-up where the metadata key will determine replication_key
-    # on a per stream basis where it could be time based or non time based
-    is_timestamp_replication_key = False
-
-    # Schema is dynamically derived on each message
-    # the target should wrap and load the data into an unstructured target
     schema = {"properties": {"_id": {"type": "string"}}}
+
+    @property
+    def is_timestamp_replication_key(self) -> bool:
+        return self.name in self.config["ts_based_replication"]
 
     def __init__(
         self, database: Database, collection_name: str, prefix: str, *args, **kwargs
@@ -58,28 +54,59 @@ class TapMongoDB(Tap):
     name = "tap-mongodb"
     config_jsonschema = th.PropertiesList(
         th.Property(
-            "database",
+            "prefix",
             th.StringType,
+            description="Optionally add a prefix for all streams, useful if ingesting \
+                from multiple shards/clusters via independent tap-mongodb configs.",
+            default="",
+        ),
+        th.Property(
+            "ts_based_replication",
+            th.ArrayType(th.StringType),
+            description="Each item should correspond to a stream name. A stream mentioned here \
+                indicates it uses timestamp-based replication. The default for a stream is \
+                non-timestamp based since Mongo most often uses epochs. This is required since \
+                the determination of timestamp based replication requires the key exist in the \
+                jsonschema with a date-like type which is impossible when there is no explicit \
+                schema prior to runtime. NOTE: Streams still require `metadata` mapping with an \
+                explicit callout of `replication-key` and `replication-method`.",
+            default=[],
+        ),
+        th.Property(
+            "mongo",
+            th.ObjectType(),
+            description="These props are passed directly to pymongo MongoClient allowing the \
+                tap user full flexibility not provided in any other Mongo tap.",
             required=True,
         ),
-        th.Property("prefix_override", th.StringType),
         th.Property("stream_maps", th.ObjectType()),
         th.Property("stream_map_settings", th.ObjectType()),
-        # Note: All other props are directly passed through to MongoClient
-        # additional_properties = {} <- TODO: MongoClient args jsonschema
     ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
-        mut_conf = {k: v for k, v in self.config.items()}
-        database = mut_conf.pop("database")
-        prefix = mut_conf.pop("prefix_override", "")
-        db = MongoClient(**mut_conf).get_database(database)
-        return [
-            CollectionStream(
-                tap=self,
-                database=db,
-                collection_name=collection,
-                prefix=(prefix or db.name).replace("-", "_"),
+        streams = []
+        client = MongoClient(**self.config["mongo"])
+        for db_name in client.list_database_names():
+            try:
+                collections = client[db_name].list_collection_names()
+            except:
+                self.logger.debug(
+                    "Skipping database %s, authenticated user does not have permission to access",
+                    db_name,
+                )
+                continue
+            for collection_name in collections:
+                prefix = self.config.get("prefix", "") + db_name.replace("-", "_")
+                streams.append(
+                    CollectionStream(
+                        tap=self,
+                        database=client[db_name],
+                        collection_name=collection_name,
+                        prefix=prefix,
+                    )
+                )
+        if not streams:
+            self.logger.error(
+                "No accessible collections found for supplied Mongo credentials"
             )
-            for collection in db.list_collection_names()
-        ]
+        return streams
